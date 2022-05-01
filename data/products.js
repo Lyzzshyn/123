c895d31e3bbf1baba8327e9a28aaa656010f4224
Hieu-FuruCRM <hieu.hong@furucrm.com>
2022-05-01 15:03:29 +0900

    init

---
+++ data/products.js
@@ -0,0 +1,186 @@
+var list_products = [{
+    "name": "Balenciaga big caro",
+    "company": "Balenciaga",
+    "img": "img/products/balenciaga-big-caro.jpg",
+    "price": "4900¥",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.0', HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "13 MP",
+        "camaraFront": "5 MP",
+        "cpu": "Qualcomm Snapdragon 425 4 nhân 64-bit",
+        "ram": "2 GB",
+        "rom": "16 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3300 mAh"
+    },
+    "masp": "BaLen01"
+}, {
+    "name": "Hermes leather bag",
+    "company": "hermes",
+    "img": "img/products/Hermes-leather-bag.jpg",
+    "price": "4900¥",
+    "star": 4,
+    "rateCount": 99,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Hermes01"
+}, {
+    "name": "balen xám bọc dây",
+    "company": "balen",
+    "img": "img/products/balenxámbọcdây.jpg",
+    "price": "6600¥",
+    "star": 6,
+    "rateCount": 88,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "Gucci xanh bọc dây",
+    "company": "Gucci",
+    "img": "img/products/Guccibọcdây.jpg",
+    "price": "4900¥",
+    "star": 5,
+    "rateCount": 300,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "chanel Mini",
+    "company": "chanel",
+    "img": "img/products/chanelMini.jpg",
+    "price": "3800¥",
+    "star": 5,
+    "rateCount": 201,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "balen To Ngang",
+    "company": "balen",
+    "img": "img/products/balenToNgang.jpg",
+    "price": "6600¥",
+    "star": 5,
+    "rateCount": 123,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "Cốc LV",
+    "company": "Louis Vuitton",
+    "img": "img/products/CốcLV.jpg",
+    "price": "4900¥",
+    "star": 4,
+    "rateCount": 456,
+    "promo": {
+        "name": "tragop",
+        "value": "0"
+    },
+    "detail": {
+        "screen": "IPS LCD, 6.26', Full HD+",
+        "os": "Android 8.1 (Oreo)",
+        "camara": "12 MP và 5 MP (2 camera)",
+        "camaraFront": "24 MP",
+        "cpu": "Qualcomm Snapdragon 660 8 nhân",
+        "ram": "4 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 512 GB",
+        "battery": "3300 mAh, có sạc nhanh"
+    },
+    "masp": "Xia0"
+}, {
+    "name": "Samsung Galaxy A8+ (2018)",
+    "company": "Samsung",
+    "img": "img/products/samsung-galaxy-a8-plus-2018-gold-600x600.jpg",
+    "price": "11.990.000",
+    "star": 4,
+    "rateCount": 35,
+    "promo": {
+        "name": "giamgia",
+        "value": "1.500.000"
+    },
+    "detail": {
+        "screen": "Super AMOLED, 6', Full HD+",
+        "img": "img/products/balenXanhbọcdây.jpg",
+        "os": "Android 7.1 (Nougat)",
+        "camara": "16 MP",
+        "camaraFront": "16 MP và 8 MP (2 camera)",
+        "cpu": "Exynos 7885 8 nhân 64-bit",
+        "ram": "6 GB",
+        "rom": "64 GB",
+        "microUSB": "MicroSD, hỗ trợ tối đa 256 GB",
+        "battery": "3500 mAh, có sạc nhanh"
+    },
+    "masp": "Sam1"
+}]
